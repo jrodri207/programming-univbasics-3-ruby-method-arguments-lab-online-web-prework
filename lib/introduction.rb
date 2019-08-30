c1458afc67208b3b3a4c494744892050258b8f9b
Joshua Rodriguez <josh030596@gmail.com>
2019-08-30 12:27:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,19 +1,19 @@
 name = "josh"
 
 def #introduction(name)
-puts "Hi, my name is #{name}." 
+puts "Hi, my name is #{name}.\n" 
 end 
 
 #introduction 
 
 def #introduction_with_language_optional(name, language = 'ruby')
-puts "Hi, my name is #{name} and I am learning to program in #{language}."
+puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
 #introduction_with_language_optional("josh") 
 
 def #introduction_with_language(name, language)
-puts "Hi, my name is #{name} and I am learning to program in #{language}."
+puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
 #introduction_with_language("josh", "elixir") 
